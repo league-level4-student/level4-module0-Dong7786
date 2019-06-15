33d1a956a4098f236c2226a01910242d1c55bfad
League Student <league@iMac.attlocal.net>
2019-06-15 15:54:20 -0700

    2D Arrays

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -17,6 +17,7 @@ public class GridPanel extends JPanel{
 	private int cols;
 	
 	//1. Create a 2D array of pixels. Do not initialize it yet.
+	Pixel[][] p;
 	
 	private Color color;
 	
@@ -34,10 +35,16 @@ public class GridPanel extends JPanel{
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
 		//2. Initialize the pixel array using the rows and cols variables.
-		
+		p = new Pixel[rows][cols];
 		
 		//3. Iterate through the array and initialize each element to a new pixel.
-		
+		for(int i = 0;i < p.length; i ++) {
+			
+			for(int k = 0; k < p[i].length; k ++) {
+				
+				p[i][k] = new Pixel(i,k);
+			}
+		}
 		
 	}
 	
@@ -48,6 +55,7 @@ public class GridPanel extends JPanel{
 	public void clickPixel(int mouseX, int mouseY) {
 		//5. Use the mouseX and mouseY variables to change the color
 		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		
 	}
 	
 	@Override
